72f65e249a870adfc3ee7aa858499e77b2e680df
Mia Hasegawa <iliaaa965@gmail.com>
2021-03-30 23:20:24 +1000

    first commit

---
+++ js/theme.js
@@ -0,0 +1,90 @@
+(function($) {
+	'use strict';
+
+	var nav_offset_top = $('header').height() + 50;
+	/*-------------------------------------------------------------------------------
+	  Navbar 
+	-------------------------------------------------------------------------------*/
+
+	//* Navbar Fixed
+	function navbarFixed() {
+		if ($('.header_area').length) {
+			$(window).scroll(function() {
+				var scroll = $(window).scrollTop();
+				if (scroll >= nav_offset_top) {
+					$('.header_area').addClass('navbar_fixed');
+				} else {
+					$('.header_area').removeClass('navbar_fixed');
+				}
+			});
+		}
+	}
+	navbarFixed();
+
+	/* ---------------------------------------------
+            Isotope js Starts
+         --------------------------------------------- */
+	$(window).on('load', function() {
+		$('.portfolio-filter ul li').on('click', function() {
+			$('.portfolio-filter ul li').removeClass('active');
+			$(this).addClass('active');
+
+			var data = $(this).attr('data-filter');
+			$workGrid.isotope({
+				filter: data
+			});
+		});
+
+		if (document.getElementById('portfolio')) {
+			var $workGrid = $('.portfolio-grid').isotope({
+				itemSelector: '.all',
+				percentPosition: true,
+				masonry: {
+					columnWidth: '.all'
+				}
+			});
+		}
+	});
+
+	/*----------------------------------------------------*/
+	/* Start Magnific Pop Up
+	/*----------------------------------------------------*/
+	if ($('.img-gal').length > 0) {
+		$('.img-gal').magnificPopup({
+			type: 'image',
+			gallery: {
+				enabled: true
+			}
+		});
+	}
+	/*----------------------------------------------------*/
+	/*  End  Magnific Pop Up
+	/*----------------------------------------------------*/
+
+	/*----------------------------------------------------*/
+	/*  Testimonials Slider
+    /*----------------------------------------------------*/
+	function testimonials_slider() {
+		if ($('.testi_slider').length) {
+			$('.testi_slider').owlCarousel({
+				loop: true,
+				margin: 30,
+				items: 2,
+				autoplay: true,
+				smartSpeed: 2500,
+				dots: true,
+				responsiveClass: true,
+				responsive: {
+					0: {
+						items: 1
+					},
+					991: {
+						items: 2
+					}
+				}
+			});
+		}
+	}
+	testimonials_slider();
+
+})(jQuery);
